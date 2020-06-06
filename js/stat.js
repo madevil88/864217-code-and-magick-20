600fede3884e6fab50c20397da137e26579600e4
madevil88 <madevil@i.ua>
2020-06-06 15:23:53 +0300

    Сделал код читабельнее и убрал дублирование

--- js/stat.js
+++ js/stat.js
@@ -10,6 +10,10 @@ var TEXT_HEIGHT = 16;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var MAX_BAR_HEIGHT = 150;
+var getTextX = function (i) {
+  return CLOUD_X + BAR_WIDTH + ((BAR_GAP + BAR_WIDTH) * i);
+};
+var textY = CLOUD_HEIGHT - FONT_GAP;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -27,10 +31,29 @@ var getMaxTime = function (arr) {
   return maxTime;
 };
 
-var getRandomSaturate = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+var getRectHeight = function (arr, i) {
+  return MAX_BAR_HEIGHT / getMaxTime(arr) * arr[i];
+};
+
+var getRandomColor = function (ctx, arr, i) {
+  var color = 'hsl(240,' + Math.round(Math.random() * 100) + '%,50%)';
+  if (arr[i] === 'Вы') {
+    color = 'rgba(255, 0, 0, 1)';
+  }
+  return ctx.fillStyle = color;
+};
+
+var renderText = function (ctx, color, arr, i) {
+  ctx.fillStyle = color;
+  if (typeof(arr[i]) === 'number') {
+    ctx.fillText(Math.round(arr[i]), getTextX(i), textY - FONT_GAP - GAP - getRectHeight(arr, i));
+  } else {
+    ctx.fillText(arr[i], getTextX(i), textY);
+  }
+};
+
+var renderColumn = function (ctx, arr, i) {
+  ctx.fillRect(CLOUD_X + BAR_WIDTH + ((BAR_GAP + BAR_WIDTH) * i), CLOUD_HEIGHT - FONT_GAP - GAP - getRectHeight(arr, i), BAR_WIDTH, getRectHeight(arr, i));
 };
 
 window.renderStatistics = function (ctx, players, times) {
@@ -44,13 +67,9 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2 + FONT_GAP);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + ((BAR_GAP + BAR_WIDTH) * i), CLOUD_HEIGHT - FONT_GAP);
-    ctx.fillStyle = 'hsl(240,' + getRandomSaturate(0, 100) + '%,50%)';
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    }
-    ctx.fillRect(CLOUD_X + BAR_WIDTH + ((BAR_GAP + BAR_WIDTH) * i), CLOUD_HEIGHT - FONT_GAP - GAP - MAX_BAR_HEIGHT / getMaxTime(times) * times[i], BAR_WIDTH, MAX_BAR_HEIGHT / getMaxTime(times) * times[i]);
-    ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_WIDTH + ((BAR_GAP + BAR_WIDTH) * i), CLOUD_HEIGHT - (FONT_GAP * 2) - GAP - MAX_BAR_HEIGHT / getMaxTime(times) * times[i]);
+    renderText(ctx, '#000', players, i);
+    getRandomColor(ctx, players, i);
+    renderColumn(ctx, times, i);
+    renderText(ctx, '#000', times, i);
   }
 };
