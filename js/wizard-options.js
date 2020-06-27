60919e4fe417add10e3dd21941409c8d0fbb651f
madevil88 <madevil@i.ua>
2020-06-27 16:19:10 +0300

    6.1.Учебный проект: pimp my mage - 1 shot

--- js/wizard-options.js
+++ js/wizard-options.js
@@ -1,6 +1,40 @@
 'use strict';
 
 (function () {
+  var COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+  var EYES_COLORS = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
+  var FIREBALL_COLORS = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
+  ];
+  var getRandomArray = function (arr) {
+    var randomArray = [];
+    for (var i = arr.length; i > 0; i--) {
+      do {
+        var index = Math.floor(Math.random() * arr.length);
+      }
+      while (randomArray.indexOf(arr[index]) !== -1);
+      randomArray.push(arr[index]);
+    }
+    return randomArray;
+  };
+
   var setup = document.querySelector('.setup');
   var setupWizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
   var setupWizardEyes = setup.querySelector('.setup-wizard .wizard-eyes');
@@ -9,9 +43,9 @@
   var inputWizardEyes = setup.querySelector('input[name="eyes-color"]');
   var inputWizardFireball = setupWizardFireball.querySelector('input[name="fireball-color"]');
 
-  var randomCoatColors = window.createWizardsList.getRandomArray(window.createWizardsList.COAT_COLORS);
-  var randomEyesColors = window.createWizardsList.getRandomArray(window.createWizardsList.EYES_COLORS);
-  var randomFireballColors = window.createWizardsList.getRandomArray(window.createWizardsList.FIREBALL_COLORS);
+  var randomCoatColors = getRandomArray(COAT_COLORS);
+  var randomEyesColors = getRandomArray(EYES_COLORS);
+  var randomFireballColors = getRandomArray(FIREBALL_COLORS);
   var index = 0;
 
   var getRandomElement = function (arr) {
